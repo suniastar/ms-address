791e20ccf8e9db3e3609cfd928d0b843096d9af7
Frederik Enste <frederik@fenste.de>
2022-10-19 01:57:05 +0200

    added post code repository func and tests

--- src/main/kotlin/de/fenste/ms/address/infrastructure/repositories/PostCodeRepository.kt
+++ src/main/kotlin/de/fenste/ms/address/infrastructure/repositories/PostCodeRepository.kt
@@ -16,16 +16,32 @@
 
 package de.fenste.ms.address.infrastructure.repositories
 
+import de.fenste.ms.address.domain.model.City
 import de.fenste.ms.address.domain.model.PostCode
+import de.fenste.ms.address.infrastructure.tables.CityTable
 import de.fenste.ms.address.infrastructure.tables.PostCodeTable
+import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.Expression
 import org.jetbrains.exposed.sql.SizedIterable
 import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.select
 import org.springframework.stereotype.Repository
 import java.util.UUID
 
 @Repository
 class PostCodeRepository {
+
+    private fun idOf(
+        code: String,
+        cityId: UUID,
+    ): EntityID<UUID>? = PostCodeTable
+        .slice(PostCodeTable.id)
+        .select { (PostCodeTable.code eq code) and (PostCodeTable.city eq cityId) }
+        .limit(1)
+        .notForUpdate()
+        .firstOrNull()
+        ?.let { p -> p[PostCodeTable.id] }
 
     fun list(
         limit: Int? = null,
@@ -38,6 +54,7 @@ class PostCodeRepository {
                 .orderBy(*order)
                 .limit(limit, offset)
                 .notForUpdate()
+
         else ->
             PostCode
                 .all()
@@ -52,4 +69,80 @@ class PostCodeRepository {
         .limit(1)
         .notForUpdate()
         .firstOrNull()
+
+    fun create(
+        code: String,
+        cityId: UUID,
+    ): PostCode {
+        require(
+            idOf(
+                code = code,
+                cityId = cityId,
+            ) == null,
+        ) { "A post code with this code and parent city does already exist." }
+
+        val city = City
+            .find { CityTable.id eq cityId }
+            .limit(1)
+            .notForUpdate()
+            .firstOrNull()
+
+        requireNotNull(city) { "The city ($cityId) does not exist" }
+
+        return PostCode.new {
+            this.code = code
+            this.city = city
+        }
+    }
+
+    fun update(
+        id: UUID,
+        code: String? = null,
+        cityId: UUID? = null,
+    ): PostCode {
+        val postCode = PostCode
+            .find { PostCodeTable.id eq id }
+            .limit(1)
+            .notForUpdate()
+            .firstOrNull()
+
+        requireNotNull(postCode) { "The post code ($id) does not exist." }
+
+        val uId = idOf(
+            code = code ?: postCode.code,
+            cityId = cityId ?: postCode.city.id.value,
+        )
+        require(
+            uId == null || uId == postCode.id,
+        ) { "A post code with this code and parent city does already exist." }
+
+        code?.let { postCode.code = code }
+        cityId?.let {
+            val city = City
+                .find { CityTable.id eq cityId }
+                .limit(1)
+                .forUpdate()
+                .firstOrNull()
+
+            requireNotNull(city) { "The city ($cityId) does not exist." }
+
+            postCode.city = city
+        }
+
+        return postCode
+    }
+
+    fun delete(
+        id: UUID,
+    ) {
+        val postCode = PostCode
+            .find { PostCodeTable.id eq id }
+            .limit(1)
+            .forUpdate()
+            .firstOrNull()
+
+        requireNotNull(postCode) { "The post code ($id) does not exist." }
+
+        postCode.delete()
+    }
 }
---
+++ src/test/kotlin/de/fenste/ms/address/infrastructure/repositories/PostCodeRepositoryTest.kt
@@ -0,0 +1,269 @@
+/*
+ * Copyright (c) 2022 Frederik Enste <frederik@fenste.de>.
+ *
+ * Licensed under the GNU General Public License, Version 3 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.fenste.ms.address.infrastructure.repositories
+
+import de.fenste.ms.address.domain.model.PostCode
+import de.fenste.ms.address.infrastructure.tables.PostCodeTable
+import de.fenste.ms.address.test.SampleData
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.util.UUID
+import kotlin.test.BeforeTest
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertContentEquals
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+@SpringBootTest
+class PostCodeRepositoryTest(
+    @Autowired private val repository: PostCodeRepository,
+) {
+
+    @BeforeTest
+    fun `set up`() {
+        SampleData.reset()
+    }
+
+    @Test
+    fun `test list on sample data`(): Unit = transaction {
+        val expected = SampleData.postCodes.sortedBy { p -> p.id.value.toString() }
+        val actual = repository.list()
+
+        assertContentEquals(expected, actual)
+    }
+
+    @Test
+    fun `test list on sample data with options`(): Unit = transaction {
+        val expected = SampleData.postCodes
+            .sortedBy { p -> p.code }
+            .drop(2)
+            .take(1)
+        val actual = repository.list(
+            order = arrayOf(PostCodeTable.code to SortOrder.ASC),
+            offset = 2,
+            limit = 1,
+        )
+
+        assertContentEquals(expected, actual)
+    }
+
+    @Test
+    fun `test list on no data`(): Unit = transaction {
+        SampleData.clear()
+        val list = repository.list()
+
+        assertTrue(list.empty())
+    }
+
+    @Test
+    fun `test find by id on sample data`(): Unit = transaction {
+        val expected = SampleData.postCodes.random()
+        val actual = repository.find(id = expected.id.value)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test find by id on no data`(): Unit = transaction {
+        SampleData.clear()
+        val actual = repository.find(id = UUID.randomUUID())
+
+        assertNull(actual)
+    }
+
+    @Test
+    fun `test find by id on non existing sample data`(): Unit = transaction {
+        val actual = repository.find(id = UUID.randomUUID())
+
+        assertNull(actual)
+    }
+
+    @Test
+    fun `test create`(): Unit = transaction {
+        val code = "CODE"
+        val city = SampleData.cities.random()
+
+        val actual = repository.create(
+            code = code,
+            cityId = city.id.value,
+        )
+
+        assertNotNull(actual.id)
+        assertEquals(code, actual.code)
+        assertEquals(city, actual.city)
+    }
+
+    @Test
+    fun `test create existing`(): Unit = transaction {
+        val city = SampleData.cities.filterNot { p -> p.postCodes.empty() }.first()
+        val code = city.postCodes.first().code
+
+        assertFailsWith<IllegalArgumentException> {
+            repository.create(
+                code = code,
+                cityId = city.id.value,
+            )
+        }
+    }
+
+    @Test
+    fun `test create non existing city`(): Unit = transaction {
+        val code = "Code"
+        val cityId = UUID.randomUUID()
+
+        assertFailsWith<IllegalArgumentException> {
+            repository.create(
+                code = code,
+                cityId = cityId,
+            )
+        }
+    }
+
+    @Test
+    fun `test update code`(): Unit = transaction {
+        val id = SampleData.postCodes.random().id.value
+        val code = "CODE"
+
+        val actual = repository.update(
+            id = id,
+            code = code,
+        )
+
+        assertNotNull(actual)
+        assertEquals(code, actual.code)
+    }
+
+    @Test
+    fun `test update city`(): Unit = transaction {
+        val postCode = SampleData.postCodes.random()
+        val city = SampleData.cities.filterNot { c -> c.postCodes.contains(postCode) }.random()
+
+        val actual = repository.update(
+            id = postCode.id.value,
+            cityId = city.id.value,
+        )
+
+        assertNotNull(actual)
+        assertEquals(city, actual.city)
+    }
+
+    @Test
+    fun `test update all`(): Unit = transaction {
+        val postCode = SampleData.postCodes.random()
+        val city = SampleData.cities.filterNot { c -> c.postCodes.contains(postCode) }.random()
+        val code = "CODE"
+
+        val actual = repository.update(
+            id = postCode.id.value,
+            code = code,
+            cityId = city.id.value,
+        )
+
+        assertNotNull(actual)
+        assertEquals(code, actual.code)
+        assertEquals(city, actual.city)
+    }
+
+    @Test
+    fun `test update all to same`(): Unit = transaction {
+        val postCode = SampleData.postCodes.random()
+        val code = postCode.code
+        val cityId = postCode.city.id.value
+
+        val actual = repository.update(
+            id = postCode.id.value,
+            code = code,
+            cityId = cityId,
+        )
+
+        assertNotNull(actual)
+        assertEquals(code, actual.code)
+        assertEquals(cityId, actual.city.id.value)
+    }
+
+    @Test
+    fun `test update nothing`(): Unit = transaction {
+        val expected = SampleData.postCodes.random()
+        val actual = repository.update(id = expected.id.value)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `test update on not existing`(): Unit = transaction {
+        val id = UUID.randomUUID()
+
+        assertFailsWith<IllegalArgumentException> {
+            repository.update(
+                id = id,
+                code = "doesn't matter",
+                cityId = UUID.randomUUID(),
+            )
+        }
+    }
+
+    @Test
+    fun `test update name to existing`(): Unit = transaction {
+        val sample = SampleData.postCodes.filter { p -> p.city.postCodes.count() >= 2 }.random()
+        val code = sample.city.postCodes.filterNot { p -> p == sample }.random().code
+
+        assertFailsWith<IllegalArgumentException> {
+            repository.update(
+                id = sample.id.value,
+                code = code,
+            )
+        }
+    }
+
+    @Test
+    fun `test update city to not existing`(): Unit = transaction {
+        val id = SampleData.postCodes.first().id.value
+        val cityId = UUID.randomUUID()
+
+        assertFailsWith<IllegalArgumentException> {
+            repository.update(
+                id = id,
+                cityId = cityId,
+            )
+        }
+    }
+
+    @Test
+    @Ignore // TODO allow cascade deletion?
+    fun `test delete`(): Unit = transaction {
+        val id = SampleData.postCodes.random().id.value
+
+        repository.delete(id)
+
+        assertNull(PostCode.findById(id))
+    }
+
+    @Test
+    fun `test delete not existing`(): Unit = transaction {
+        val id = UUID.randomUUID()
+
+        assertFailsWith<IllegalArgumentException> {
+            repository.delete(id)
+        }
+    }
+}
